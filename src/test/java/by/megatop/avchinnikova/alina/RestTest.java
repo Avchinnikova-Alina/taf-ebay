1b108fd1cd77bf660ade9407e30ea05a174b399e
Sergei Tsarik <serge.tsarik@gmail.com>
2023-07-11 21:54:00 +0300

    refactor

--- src/test/java/by/megatop/avchinnikova/alina/RestTest.java
+++ src/test/java/by/megatop/avchinnikova/alina/RestTest.java
@@ -2,15 +2,19 @@ package by.megatop.avchinnikova.alina;
 
 import io.restassured.response.ValidatableResponse;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestTest {
 
+    @Ignore
     @Test
     public void testGetLogin() {
         given().when().get("https://my.megatop.by/login").then();
+        Assert.fail("Нечего проверять");
     }
 
     @Test
@@ -44,13 +48,22 @@ public class RestTest {
     @Test
     public void testPostFromWithIncorrectEmail() {
         PostObject po = new PostObject("117858875", "qwerty1Q");
-        ValidatableResponse response = given().header("Content-Type", "application/json").body(po).
-                when().post("https://admin.megatop.by/api/v1/user/login").then();
+        given().header("Content-Type", "application/json").body(po).
+                when().post("https://admin.megatop.by/api/v1/user/login").then().
+                assertThat().
+                    statusCode(422).
+                    body("message", equalTo("Вы ввели неверный номер телефона и/или пароль")).
+                    body("errors.phone[0]", equalTo("Вы ввели неверный номер телефона и/или пароль")).
+                    body("status", equalTo("error"));
+
+
+/*
         Assert.assertEquals(response.extract().statusCode(), 422);
         Assert.assertEquals("\u0412\u044B \u0432\u0432\u0435\u043B\u0438 \u043D\u0435\u0432\u0435\u0440\u043D" +
                         "\u044B\u0439 \u043D\u043E\u043C\u0435\u0440 \u0442\u0435\u043B\u0435\u0444\u043E\u043D\u0430 " +
                         "\u0438/\u0438\u043B\u0438 \u043F\u0430\u0440\u043E\u043B\u044C",
                 "Вы ввели неверный номер телефона и/или пароль");
+*/
     }
 
     @Test
